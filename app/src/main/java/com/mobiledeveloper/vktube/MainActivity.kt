d7f204986adb54f9cd7cfd03aaee32f1ec0ad528
Dzhos Oleksii <oleksii.dzhos@ukma.edu.ua>
2022-03-13 13:38:34 +0200

    Use launcherForActivityResult after onActivityResult

--- app/src/main/java/com/mobiledeveloper/vktube/MainActivity.kt
+++ app/src/main/java/com/mobiledeveloper/vktube/MainActivity.kt
@@ -1,28 +1,17 @@
 package com.mobiledeveloper.vktube
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.mobiledeveloper.vktube.navigation.NavigationGraph
 import com.mobiledeveloper.vktube.ui.theme.Fronton
 import com.mobiledeveloper.vktube.ui.theme.FrontonTheme
-import com.vk.api.sdk.VK
-import com.vk.api.sdk.auth.VKAccessToken
-import com.vk.api.sdk.auth.VKAuthCallback
-import com.vk.api.sdk.exceptions.VKAuthException
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.MutableStateFlow
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private var vkAuthResultToken: MutableStateFlow<String> = MutableStateFlow("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,22 +31,6 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 NavigationGraph()
             }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val callback = object : VKAuthCallback {
-            override fun onLogin(token: VKAccessToken) {
-                vkAuthResultToken.value = token.accessToken
-            }
-
-            override fun onLoginFailed(authException: VKAuthException) {
-                Log.e("TAG", "exception ${authException.authError}")
-            }
-        }
-
-        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
-            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
