c0247e87342490a09d315d19d9854bca719bfcb4
Artem Slinkin <slartus@gmail.com>
2022-04-07 15:27:30 +0500

    comparator fix

--- app/src/main/java/com/mobiledeveloper/vktube/ui/screens/feed/VideoCellModelComparator.kt
+++ app/src/main/java/com/mobiledeveloper/vktube/ui/screens/feed/VideoCellModelComparator.kt
@@ -5,7 +5,7 @@ import com.mobiledeveloper.vktube.ui.screens.feed.models.VideoCellModel
 internal class VideoCellModelComparator : Comparator<VideoCellModel> {
     override fun compare(m0: VideoCellModel, m1: VideoCellModel): Int {
         return if (m0.groupInfo.id == m1.groupInfo.id) {
-            m0.groupOrder.compareTo(m1.groupInfo.id)
+            m0.groupOrder.compareTo(m1.groupOrder)
         } else {
             -m0.dateAdded.compareTo(m1.dateAdded)
         }
